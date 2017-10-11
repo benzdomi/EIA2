0ff005a88992b50f8d1961c5f84ba0aa3579aa01
lilbe <lilbe@LAPTOP-5DUE55H4.Speedport_W_724V_09011603_05_012>
2017-10-11 14:30:25 +0200

    miniseite

---
+++ Aufgabe 0/Aufgabe0.js
@@ -0,0 +1,17 @@
+/*
+Aufgabe: Aufgabe 0
+Name: Dominik Benz
+Matrikel: 256152
+Datum: 11.10.2017
+Hiermit versichere ich, dass ich diesen
+Code selbst geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert.
+*/
+window.addEventListener("load", input);
+function input() {
+    let name = prompt("Geben sie ihren Namen ein.", "");
+    if (name != null) {
+        alert("Hallo " + name + "!");
+    }
+}
+//# sourceMappingURL=Aufgabe0.js.map
---
+++ Aufgabe 0/Aufgabe0.ts
@@ -0,0 +1,16 @@
+/*
+Aufgabe: Aufgabe 0
+Name: Dominik Benz
+Matrikel: 256152
+Datum: 11.10.2017
+Hiermit versichere ich, dass ich diesen
+Code selbst geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert.
+*/
+window.addEventListener("load", input);
+function input(): void {
+    let name: string = prompt("Geben sie ihren Namen ein.", "");
+    if (name != null) {
+          alert("Hallo " + name + "!");
+    }
+}
