dde17752dc517626ce3ee37c79d79c3bd30fb514
benzdomi <32565941+benzdomi@users.noreply.github.com>
2017-12-15 09:45:46 +0100

    Add files via upload

--- Aufgabe 8/main.js
+++ Aufgabe 8/main.js
@@ -1,22 +1,33 @@
+/*
+Aufgabe: Aufgabe 8
+Name: Dominik Benz
+Matrikel: 256152
+Datum: 14.12.2017
+Hiermit versichere ich, dass ich diesen
+Code selbst geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert.
+*/
 var Aufgabe8;
 (function (Aufgabe8) {
     window.addEventListener("load", input);
     function input() {
         var anzahl = prompt("Geben Sie eine Zahl zwischen 10 und 100 ein.");
-        if (Number.isNaN(parseInt(anzahl)) || parseInt(anzahl) < 10 || parseInt(anzahl) > 100) {
+        var breite = prompt("Geben Sie eine Breite ein.");
+        var hohe = prompt("Geben Sie eine H�he ein.");
+        if (Number.isNaN(parseInt(anzahl)) || Number.isNaN(parseInt(breite)) || Number.isNaN(parseInt(hohe)) || parseInt(anzahl) < 10 || parseInt(anzahl) > 100) {
             alert("Eingabe ist falsch");
             input();
         }
         else {
             for (let i = 0; i < parseInt(anzahl); i++) {
-                init(Math.random() * window.innerWidth - 50, Math.random() * window.innerHeight - 50, Math.random() * 360);
+                init(Math.random() * window.innerWidth - parseInt(breite), Math.random() * window.innerHeight - parseInt(hohe), Math.random() * 360, parseInt(breite), parseInt(hohe));
             }
         }
     }
-    function init(_x, _y, _color) {
+    function init(_x, _y, _color, _width, _height) {
         let box = document.createElement("div");
-        box.style.width = "50px";
-        box.style.height = "50px";
+        box.style.width = _width + "px";
+        box.style.height = _height + "px";
         box.style.backgroundColor = "hsl(" + _color + ", 100%, 50%)";
         box.style.left = _x + "px";
         box.style.top = _y + "px";
--- Aufgabe 8/main.ts
+++ Aufgabe 8/main.ts
@@ -1,24 +1,35 @@
+/*
+Aufgabe: Aufgabe 8
+Name: Dominik Benz
+Matrikel: 256152
+Datum: 14.12.2017
+Hiermit versichere ich, dass ich diesen
+Code selbst geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert.
+*/
 namespace Aufgabe8 {
     window.addEventListener("load", input);
 function input(): void {
     var anzahl: string = prompt("Geben Sie eine Zahl zwischen 10 und 100 ein.");
-    if (Number.isNaN(parseInt(anzahl)) || parseInt(anzahl) < 10 || parseInt(anzahl) > 100) {
+    var breite: string = prompt("Geben Sie eine Breite ein.");
+    var hohe: string = prompt("Geben Sie eine Höhe ein.");
+    if (Number.isNaN(parseInt(anzahl)) || Number.isNaN(parseInt(breite)) || Number.isNaN(parseInt(hohe)) || parseInt(anzahl) < 10 || parseInt(anzahl) > 100) {
         alert("Eingabe ist falsch");
         input();
         }else {
             for ( let i: number = 0; i < parseInt(anzahl); i++) {
-                init(Math.random() * window.innerWidth - 50, Math.random() * window.innerHeight - 50, Math.random() * 360);
+                init(Math.random() * window.innerWidth - parseInt(breite), Math.random() * window.innerHeight - parseInt(hohe), Math.random() * 360, parseInt(breite), parseInt(hohe));
                 }
         }
     }
     
-    function init(_x: number, _y: number, _color: number): void {
+    function init(_x: number, _y: number, _color: number, _width: number, _height: number): void {
 
             let box: HTMLDivElement = document.createElement("div");
 
             
-            box.style.width = "50px";
-            box.style.height = "50px";
+            box.style.width = _width + "px";
+            box.style.height = _height + "px";
             box.style.backgroundColor = "hsl(" + _color + ", 100%, 50%)";
             box.style.left = _x + "px";
             box.style.top = _y + "px";
